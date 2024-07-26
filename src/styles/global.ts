ce0d7e83d638c72dc81c0b02f1ff34028639763f
Tiago Souza <tiago0214@gmail.com>
2024-07-26 02:00:01 +0100

    create general styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,16 +1,27 @@
 import { createGlobalStyle } from "styled-components";
 
+import img from './../assets/bg-texture.png'
+
 export const GlobalStyle = createGlobalStyle`
   *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    outline: 0;
+    list-style: none;
+    text-decoration: none;
   }
 
   body{
-    background-color: ${props => props.theme["gray-900"]};
+    background-color: ${props => props.theme["color-bg"]};
     color: ${props => props.theme.white};
+    line-height: 1.7;
     -webkit-font-smoothing: antialiased;
+    background-image: url(${img});
+  }
+
+  html{
+    scroll-behavior: smooth;
   }
 
   :focus{
@@ -23,4 +34,95 @@ export const GlobalStyle = createGlobalStyle`
     font-size: 1rem;
   }
 
+  ::-webkit-scrollbar{
+    display: none;
+  }
+
+  .container{
+    width: ${props => props.theme["container-width-lg"]};
+    margin: 0 auto;
+  }
+
+  h1,h2,h3,h4,h5{
+    font-weight: 500;
+  }
+
+  h1{
+    font-size: 2.5rem;
+  }
+
+  section{
+    margin-top: 8rem;
+  }
+
+  section > h2, section > h5{
+    text-align: center;
+    color: ${props => props.theme.light}
+  }
+
+  section > h2 {
+    color: ${props => props.theme.primary};
+    margin-bottom: 3rem;
+  }
+
+  .text-light{
+    color: ${props => props.theme.light}
+  }
+
+  a{
+    color: ${props => props.theme.primary};
+    transition: ${props => props.theme.transition};
+
+    &:hover{
+    color: ${props => props.theme.white};
+    }
+  }
+
+  .bnt{
+    width: max-content;
+    display: inline-block;
+    color: ${props => props.theme.primary};
+    padding: 0.75rem 1.2rem;
+    border-radius: 0.4rem;
+    cursor: pointer;
+    border: 1px solid ${props => props.theme.primary};
+    transition: ${props => props.theme.transition};
+
+    &:hover{
+    background-color: ${props => props.theme.white};
+    color: ${props => props.theme["color-bg"]};
+    border-color: transparent;
+    }
+  }
+
+  .btn-primary{
+    background: ${props => props.theme.primary};
+    color: ${props => props.theme["color-bg"]}
+  }
+
+  img{
+    display: block;
+    width: 100%;
+    object-fit: cover;
+  }
+
+  @media screen and (max-width: 1024px){
+    .container{
+      width: ${props => props.theme["container-width-md"]};
+    }
+
+    section{
+      margin-top: 6rem;
+    }
+  }
+
+  @media screen and (max-width: 600px){
+    .container{
+      width: ${props => props.theme["container-width-sm"]};
+    }
+
+    section > h2{
+      margin-bottom: 2rem;
+    }
+  }
 `;
--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,21 +1,14 @@
 export const defaultTheme = {
   white: '#FFF',
+  transition: 'all 400ms ease',
 
-  'gray-100': '#E1E1E6',
-  'gray-300': '#C4C4CC',
-  'gray-400': '#8D8D99',
-  'gray-500': '#7C7C8A',
-  'gray-600': '#323238',
-  'gray-700': '#29292E',
-  'gray-800': '#202024',
-  'gray-900': '#121214',
-
-  'green-300': '#00B37E',
-  'green-500': '#00875F',
-  'green-700': '#015F43',
-
-  'red-500': '#AB222E',
-  'red-700': '#7A1921',
-
-  'yellow-500': '#FBA94C',
+  'color-bg': '#1f1f38',
+  'bg-variant': '#2c2c6c',
+  'primary': '#4db5ff',
+  'primary-variant': 'rgba(77,181,255,0.4)',
+  'light': 'rgba(255,255,255,0.6)',
+  
+  'container-width-lg': '75%',
+  'container-width-md': '86%',
+  'container-width-sm': '90%',
 } as const
