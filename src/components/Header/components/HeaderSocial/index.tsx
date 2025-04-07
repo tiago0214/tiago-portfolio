084b8a9c6cf7ead44674e011d56c0b70221159c0
Tiago Souza <tiago0214@gmail.com>
2025-04-06 23:52:56 -0300

    feat/change linkedin link

--- src/components/Header/components/HeaderSocial/index.tsx
+++ src/components/Header/components/HeaderSocial/index.tsx
@@ -6,8 +6,8 @@ import { motion } from "framer-motion";
 export function HeaderSocial(){
   return (
     <HeaderSocialContainer>
-    <motion.a  whileHover={{scale:1.5}} href="https://www.linkedin.com/in/tiago-souza-ba234b11a/" target="_blank"> <LinkedInIcon sx={{ fontSize: 24 }}/> </motion.a>
-    <motion.a  whileHover={{scale:1.5}}  href="https://github.com/tiago0214" target="_blank"> <GitHubIcon sx={{ fontSize: 24 }}/> </motion.a>
+      <motion.a  whileHover={{scale:1.5}} href="https://www.linkedin.com/in/tiagodias-dev/" target="_blank"> <LinkedInIcon sx={{ fontSize: 24 }}/> </motion.a>
+      <motion.a  whileHover={{scale:1.5}}  href="https://github.com/tiago0214" target="_blank"> <GitHubIcon sx={{ fontSize: 24 }}/> </motion.a>
     </HeaderSocialContainer>
   )
 }
